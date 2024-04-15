b662ea92451c7ef7753609116ec3c965b491259b
Carla Lucio <103859003+carlalucio@users.noreply.github.com>
2024-04-15 14:35:49 -0300

    adicionando o schema.yml

--- demo.tf
+++ demo.tf
@@ -2,17 +2,9 @@ provider "oci" {
     region = var.region
 }
 
-variable "compartment_ocid" {
-    default = "ocid1.compartment.oc1..aaaaaaaazlipe6cgz7ngxjie42czhhtqtl6aseqwitiehd2v3ia2xsbj7eya"
-}
-
-variable "region" {
-    default = "us-ashburn-1"
-}
-
-variable "bucket_name" {
-    default = "resource_manager_demo_bucket"
-}
+variable "compartment_ocid" {}    
+variable "region" {}
+variable "bucket_name" {}
 
 data "oci_objectstorage_namespace" "namespace" {
     compartment_id = var.compartment_ocid
